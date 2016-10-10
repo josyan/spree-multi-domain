e23ff59f3bb538b5dff7fb3833e820cccaac9a41
drakkensaer <shadowdrakkeen@gmail.com>
2016-10-10 19:09:58 +0000

    update

--- db/migrate/20161010183420_add_columns_to_stores.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToStores < ActiveRecord::Migration
-  def change
-    add_column :spree_stores, :logo_content_type, :string
-    add_column :spree_stores, :logo_file_size, :integer
-  end
-end
